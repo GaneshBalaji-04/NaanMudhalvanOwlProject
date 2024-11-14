1d337715819e11990398bc02c934aa126851a85c
GaneshBalaji-04 <149702079+GaneshBalaji-04@users.noreply.github.com>
2024-11-14 12:46:04 +0530

    Add files via upload

---
+++ src/main/java/com/example/owlapplication/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.owlapplication.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFF6A1B9A) // New shade of purple
+val Purple500 = Color(0xFFAB47BC) // Vibrant purple shade
+val Purple700 = Color(0xFF8E24AA) // Darker purple tone
+val Teal200 = Color(0xFF00ACC1)   // New teal color
